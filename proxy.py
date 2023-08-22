78f7a14a5ec8d7d05ca26aadf57e71aa20001415
wdotmathree <wdotmathree@gmail.com>
2023-08-21 23:09:37 -0600

    Start work on IKEv2 and other boilerplate
    Wrote scripts to help with debugging and wrote a little in the IPSec
    file to help with future development

---
+++ proxy.py
@@ -0,0 +1,27 @@
+# Proxy for debugging (idk how to debug as root)
+
+from socket import *
+from time import sleep
+
+down = socket(AF_INET, SOCK_DGRAM)
+up = socket(AF_INET, SOCK_DGRAM)
+
+down.bind(("0.0.0.0", 500))
+up.bind(("127.0.0.1", 9500))
+down.setblocking(0)
+up.setblocking(0)
+
+daddr, uaddr = ((),("127.0.0.1", 5000))
+
+while True:
+    try:
+        buf, daddr = down.recvfrom(65535)
+        up.sendto(buf, uaddr)
+    except BlockingIOError:
+        pass
+    try:
+        buf, uaddr = up.recvfrom(65535)
+        down.sendto(buf, daddr)
+    except BlockingIOError:
+        pass
+    sleep(0.001)
